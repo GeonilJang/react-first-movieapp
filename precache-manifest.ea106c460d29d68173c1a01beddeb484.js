ef36613ced32163df0a140c0f4ab52cb5134f88e
geonil <jgi92@naver.com>
2019-02-09 22:59:36 +0900

    Updates

---
+++ precache-manifest.ea106c460d29d68173c1a01beddeb484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7d10bba8c01b9f3d022",
+    "url": "/movieapp/static/js/runtime~main.e7d10bba.js"
+  },
+  {
+    "revision": "c923cd6d64c9393cb548",
+    "url": "/movieapp/static/js/main.c923cd6d.chunk.js"
+  },
+  {
+    "revision": "ba59fe894365b859e575",
+    "url": "/movieapp/static/js/1.ba59fe89.chunk.js"
+  },
+  {
+    "revision": "c923cd6d64c9393cb548",
+    "url": "/movieapp/static/css/main.6acc5e9c.chunk.css"
+  },
+  {
+    "revision": "a98d25ae376cb3e82e0c45a3be768cb0",
+    "url": "/movieapp/index.html"
+  }
+];
